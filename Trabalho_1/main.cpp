c250c346d3bd3b00a7702a72aba0adf2ddba1b61
Gabriel Rodrigues Malaquias <48767872+gabriel61944307@users.noreply.github.com>
2019-10-28 11:03:54 -0300

    Create main.cpp

---
+++ Trabalho_1/main.cpp
@@ -0,0 +1,51 @@
+/*  Trabalho 1 de Programação de computadores.
+ *  
+ * Gabriel Rodrigues Malaquias, 758906, 2018.
+ */
+
+#include <iostream>
+#include "Aluno.h"
+#include "Data.h"
+#include "Voo.h"
+#include "Conversor.h"
+
+using namespace std;
+
+int main(){
+    /*
+    //ALUNO
+    Aluno a1(758906, "Gabriel", 6, 5, 5);
+    //a1.imprime();
+
+    // DATA
+    Data d1(8, 7, 2000);
+    d1.imprime();
+
+    Data d2(15, 7, 2000);
+    d2.imprime();
+
+    d2.compara(d1);
+
+    //VOO
+    Voo v1(1, d2, "11:00");
+    v1.verifica(5);
+    v1.ocupa(5);
+    v1.ocupa(1);
+    v1.ocupa(2);
+    v1.ocupa(3);
+    v1.ocupa(4);
+    v1.ocupa(7);
+    v1.proximoLivre();
+    v1.desocupa(8);
+    v1.desocupa(7);
+
+    v1.imprime();
+
+    //CONVERSOR
+    Conversor c1(3.17, 3.01);
+    c1.imprimeTaxas();
+    c1.compraDolar(5);
+    c1.vendeDolar(1);
+    */
+    return 0;
+}
