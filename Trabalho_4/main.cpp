b41d7a2fb000ad628763c9dc50befc067867f67e
Gabriel Rodrigues Malaquias <48767872+gabriel61944307@users.noreply.github.com>
2019-10-28 11:28:51 -0300

    Create main.cpp

---
+++ Trabalho_4/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Trabalho 4 de programação de computadores
+ * 
+ * Gabriel Rodrigues Malaquias, 758906, 2018.
+ */
+
+#include <iostream>
+#include "Cesar.h"
+
+using namespace cifra;
+
+int main() {
+    //Cesar* c = new Cesar();
+    //c->encripte("entrada.txt", "saida.txt", 7);
+    //c->encripte("entrada.txt", "saida.txt", -15);
+    return 0;
+}
